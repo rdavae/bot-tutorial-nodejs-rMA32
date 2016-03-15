f15accbb0909fb4cf7b2c9a146873b4f3a142ef4
rdavae <rajdavae15@gmail.com>
2016-03-15 14:41:42 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -12,7 +12,7 @@ function respond() {
       botRegexO = /^\/orgy/;
       botRegexyub = /^\/y u b/;
       botRegexj = /^\/joaje/;
-      botRegexj = /^\/help/;
+      botRegexhelp = /^\/help/;
       
       siege1 = 'https://i.groupme.com/350x419.png.adc8c73a6c1547e0a9e04320296329f8'; siege2 = 'https://i.groupme.com/1279x752.jpeg.aa5d0401e0df495bba4b4e09dc5a6bd7'
       siege3 = 'https://i.groupme.com/960x960.png.006e180e05d841c6a2962e844bf1e6fd';
