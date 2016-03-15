edbd5a245db2e7f00d8391ccf9cf8dc9f418240b
rdavae <rajdavae15@gmail.com>
2016-03-15 14:32:34 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,6 +11,8 @@ function respond() {
       botRegexWG = /^\/what a guy/;
       botRegexO = /^\/orgy/;
       botRegexyub = /^\/y u b/;
+      botRegexj = /^\/joaje/;
+      http://i.imgur.com/Bq9o2U4.gif
       
       siege1 = 'https://i.groupme.com/350x419.png.adc8c73a6c1547e0a9e04320296329f8'; siege2 = 'https://i.groupme.com/1279x752.jpeg.aa5d0401e0df495bba4b4e09dc5a6bd7'
       siege3 = 'https://i.groupme.com/960x960.png.006e180e05d841c6a2962e844bf1e6fd';
@@ -23,6 +25,11 @@ function respond() {
    else if(request.text && botRegexDB.test(request.text)) {
     this.res.writeHead(200);
     postMessage("http://i.imgur.com/fJv4zO0.gif");
+    this.res.end();
+  } 
+   else if(request.text && botRegexj.test(request.text)) {
+    this.res.writeHead(200);
+    postMessage("http://i.imgur.com/Bq9o2U4.gif");
     this.res.end();
   } 
    else if(request.text && botRegexO.test(request.text)) {
