003721932ed5df64133e8d78c5848c1277aa8c3f
rdavae <rajdavae15@gmail.com>
2016-03-17 13:48:21 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -64,16 +64,7 @@ function respond() {
      this.res.writeHead(200);
      postMessage("What\'s an  ?");
       this.res.end();
-    var pos = request.text.search(/\bass\b/i);
-    if(request.text.length() > 3){
     
-    var memes = request.text.substring(pos + 3, str.length);
-    memes = memes.trim();
-   var p2 = memes.search(/ /);
-    var m2 = memes.substring(0, p2);
-    
-    
-    }
    
   } 
     else if(request.text && botRegexns.test(request.text)) {
