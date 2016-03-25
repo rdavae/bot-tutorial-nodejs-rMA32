5dbc821b1c6be0d7d888063b4755a39015bd6b43
rdavae <rajdavae15@gmail.com>
2016-03-24 21:14:39 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -24,6 +24,7 @@ botRegexmm = /^\/millerdid310|\/itwasanunderwaterjob/;
       botRegextank = /\btank\b/i;
       botRegexass = /\bass\b/i;
       botRegexweed = /^\/snoop/;
+      botRegexweh = /^\/weh/;
       siege1 = 'https://i.groupme.com/350x419.png.adc8c73a6c1547e0a9e04320296329f8'; siege2 = 'https://i.groupme.com/1279x752.jpeg.aa5d0401e0df495bba4b4e09dc5a6bd7'
       siege3 = 'https://i.groupme.com/960x960.png.006e180e05d841c6a2962e844bf1e6fd';
 
@@ -112,6 +113,13 @@ botRegexmm = /^\/millerdid310|\/itwasanunderwaterjob/;
     this.res.end();
     
   } 
+  else if(request.text && botRegexweh.test(request.text)) {
+    this.res.writeHead(200);
+    postMessage("http://i.imgur.com/MYaFZYv.png");
+    this.res.end();
+    
+
+  }
    else if(request.text && botRegexla.test(request.text)) {
     this.res.writeHead(200);
     postMessage("http://i.imgur.com/XtliRDG.png");
